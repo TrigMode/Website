c641dbada044362fef44b11d8785fba0a6001101
Xtriger <aman584867@gmail.com>
2021-04-01 18:34:29 -0700

    Add files via upload

---
+++ LY2/js/abouttxt.js
@@ -0,0 +1,4 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.font = "18px Arial";
+ctx.fillText("Hi, I'm Aman 'Trigger' Nandhra. I'm a student who's probably gonna do something related to Computer Science. I'm your average brown boy", 100, 100);
